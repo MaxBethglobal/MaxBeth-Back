1fb31408cff75c5e27e63e0743a1a0d65e1495fb
EwanDebove <ewan.debove@gmail.com>
2022-02-06 17:06:50 +0100

    changed data struct to allow multiple events

--- contracts/BettingEvent.sol
+++ contracts/BettingEvent.sol
@@ -78,22 +78,25 @@ contract Betting is ERC1155Supply, Ownable {
     }
 
     // Bet
-    function bet(uint256 resultId_) public payable returns (bool) {
-        require(status == State.OPEN, "Betting: Event not open");
+    function bet(uint256 eventId_, uint256 resultId_) public payable returns (bool) {
+        require(bettingEvents[eventId_].status == State.OPEN, "Betting: Event not open");
         // TODO require a minimum bet amount
 
         // transfer & register the bet
         // safeTransferFrom(address(this), msg.sender, resultId_, msg.value, "0x0");
 
         // another way
-        require(resultsIds[resultId_], "Betting: This result does not exist!");
+        require(bettingEvents[eventId_].resultsIds[resultId_], "Betting: This result does not exist!");
         _mint(msg.sender, resultId_, msg.value, "");
 
         return true;
     }
     
-    function batchBet(uint256[] memory resultIds_, uint256[] memory amounts) public payable returns (bool) {
-        require(status == State.OPEN, "Betting: Event not open");
+    function batchBet(uint256 eventId_, uint256[] memory resultIds_, uint256[] memory amounts) public payable returns (bool) {
+        // for (uint i = 0; i < eventIds_.length; i++){
+        //     require(bettingEvents[eventIds_[i]].status == State.OPEN, "Betting: Event not open");
+        // }
+        require(bettingEvents[eventId_].status == State.OPEN, "Betting: Event not open");
         // TODO require a minimum total bet amounts
 
         uint amountsTotal = 0;
@@ -105,33 +108,40 @@ contract Betting is ERC1155Supply, Ownable {
         // safeBatchTransferFrom(address(this), msg.sender, resultIds_, amounts, "0x0");
         
         // another way
-        for (uint i = 0; i < resultIds_.length; i++) {
-            require(resultsIds[resultIds_[i]], "Betting: This result does not exist!");
+        for (uint i = 0; i < bettingEvents[eventId_].resultsIdsKeys.length; i++) {
+            require(bettingEvents[eventId_].resultsIds[resultIds_[i]], "Betting: This result does not exist!");
         }
         _mintBatch(msg.sender, resultIds_, amounts, "");
     
         return true;
     }
 
+    function batchEventBet(uint256[] memory eventIds_, uint256[][] memory resultIds_, uint256[][] memory amounts) public payable returns (bool){
+        for (uint i = 0; i < eventIds_.length; i++){
+            batchBet(eventIds_[i], resultIds_[i], amounts[i]);
+        }
+        
+        return true;
+    }
     function lock() external {
         status = State.LOCKED;
     }
 
     // Store the event result
-    function store(uint256 resultId_) public returns (bool) {
+    function store(uint256 eventId_, uint256 resultId_) public returns (bool) {
         // Require accessControl
         require(exists(resultId_), "Betting: This result does not exist!");
 
-        finalResult = resultId_;
-        status = State.ENDED;
+        bettingEvents[eventId_].finalResult = resultId_;
+        bettingEvents[eventId_].status = State.ENDED;
 
         return true;
     }
 
-    function totalBetsOnEvent() public view returns (uint256) {
+    function totalBetsOnEvent(uint256 eventId_) public view returns (uint256) {
         uint _totalBetsOnEvent = 0;
-        for (uint i = 0; i < resultsIdsKeys.length; i++) {
-            _totalBetsOnEvent += totalSupply(resultsIdsKeys[i]);
+        for (uint i = 0; i < bettingEvents[eventId_].resultsIdsKeys.length; i++) {
+            _totalBetsOnEvent += totalSupply(bettingEvents[eventId_].resultsIdsKeys[i]);
         }
 
         return _totalBetsOnEvent;
@@ -141,23 +151,23 @@ contract Betting is ERC1155Supply, Ownable {
     /// @dev Compute potential or final winnings
     /// @param resultId_ result id
     /// @return the potential or final winnings of msg.sender for a result id
-    function computeWinnings(uint256 resultId_) public view returns (uint256) {
+    function computeWinnings(uint256 eventId_, uint256 resultId_) public view returns (uint256) {
         require(totalSupply(resultId_) != 0, "Betting: no bets on event");
         // winnings = balanceOf(msg.sender, resultId_) * (1 + total_bets_on_resultId_ / total_bets_on_event)
         
-        uint256 winnings = balanceOf(msg.sender, resultId_) * totalBetsOnEvent() / totalSupply(resultId_);
+        uint256 winnings = balanceOf(msg.sender, resultId_) * totalBetsOnEvent(eventId_) / totalSupply(resultId_);
         winnings *= (10000 - fee)/10000;
 
         return winnings;
     }
     
     // claim / cash out single Id
-    function cashOut(uint256 resultId_) public returns (bool) {
+    function cashOut(uint256 eventId_, uint256 resultId_) public returns (bool) {
         require(balanceOf(msg.sender, resultId_) > 0, "Betting: You didn't bet on this");
-        require(status == State.ENDED, "Betting: Event not over");
-        require(resultId_ == finalResult, "Betting: No winnings");
+        require(bettingEvents[eventId_].status == State.ENDED, "Betting: Event not over");
+        require(resultId_ == bettingEvents[eventId_].finalResult, "Betting: No winnings");
 
-        uint winnings = computeWinnings(resultId_);
+        uint winnings = computeWinnings(eventId_, resultId_);
 
         payable(msg.sender).transfer(winnings);
 
@@ -166,10 +176,20 @@ contract Betting is ERC1155Supply, Ownable {
 
     // TODO
     // cash out multiple ids / all results for msg.sender
-    function batchCashOut(uint256[] memory resultIds_) public returns (bool) {
-        
-    }
+    function batchCashOut(uint256 eventIds_, uint256[] memory resultIds_) public returns (bool) {
+        for (uint i = 0; i < resultIds_.length; i++){
+            cashOut(eventIds_, resultIds_[i]);
+        }
 
+        return true;
+    }
+    function batchEventCashOut(uint256[] memory eventIds_, uint256[][] memory resultIds_) public returns (bool) {
+        for (uint i = 0; i < eventIds_.length; i++){
+            batchCashOut(eventIds_[i], resultIds_[i]);
+        }
+        
+        return true;
+    }
     // withdraw : not production code !
     function withdraw(uint256 value_) public onlyOwner() {
         // transfer the fees value TODO
