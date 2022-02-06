3aaa8dfed4ef8b82d0581811b175fda2f30842b2
EwanDebove <ewan.debove@gmail.com>
2022-02-06 17:23:44 +0100

    cleanup

--- contracts/BettingEvent.sol
+++ contracts/BettingEvent.sol
@@ -6,13 +6,8 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
-// import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
-// import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
 
-// Event
-// result1, result2...
 
-// contract BettingEvent is ERC1155Supply, ERC1155Holder, Ownable {
 /// @title A title that should describe the contract/interface
 /// @author The name of the author
 /// @notice Explain to an end user what this does
@@ -32,39 +27,12 @@ contract Betting is ERC1155Supply, Ownable {
     }
     mapping(uint256 => BettingEvent) public bettingEvents;
     uint256[] public bettingEventsKeys;
-    //TODO add event ids in functions (param...)
 
-    // mapping(uint256 => uint256) public finalResult4Ev;
-    // mapping(uint256 => mapping(uint256 => bool)) public resultsIds4Ev;
-    // mapping(uint256 => uint256[]) public resultsIdsKeys4Ev;
-
-    State public status;
-    uint256 public finalResult;
-    mapping(uint256 => bool) public resultsIds;
-    uint256[] public resultsIdsKeys;
     // fee or "rake" :
     uint256 public fee = 100;
 
-    // "https://ourserver/api/event/{id}.json"
-    // constructor(string memory uri_) ERC1155(uri_) {
-    // }
 
-    // constructor(string memory uri_, 
-    //     uint256[] memory ids_,
-    //     uint256[] memory amounts_,
-    //     bytes memory data_) ERC1155(uri_) {
-        
-    //     // _mintBatch(msg.sender, ids_, amounts_, data_);
-    //     // ApprovalForAll(msg.sender, address(this), true);
-        
-    //     mintBatch(ids_, amounts_, data_);
-    // }
 
-    // function mintBatch(uint256[] memory ids_, uint256[] memory amounts_, 
-    //     bytes memory data_) public onlyOwner() {
-    //     _mintBatch(address(this), ids_, amounts_, data_);
-    // }
-    
     constructor(string memory uri_, uint256[] memory resultIds_) ERC1155(uri_) {
         addResultIds(resultIds_);
         status = State.OPEN;
@@ -77,15 +45,11 @@ contract Betting is ERC1155Supply, Ownable {
         }
     }
 
-    // Bet
+
     function bet(uint256 eventId_, uint256 resultId_) public payable returns (bool) {
         require(bettingEvents[eventId_].status == State.OPEN, "Betting: Event not open");
         // TODO require a minimum bet amount
 
-        // transfer & register the bet
-        // safeTransferFrom(address(this), msg.sender, resultId_, msg.value, "0x0");
-
-        // another way
         require(bettingEvents[eventId_].resultsIds[resultId_], "Betting: This result does not exist!");
         _mint(msg.sender, resultId_, msg.value, "");
 
@@ -93,9 +57,6 @@ contract Betting is ERC1155Supply, Ownable {
     }
     
     function batchBet(uint256 eventId_, uint256[] memory resultIds_, uint256[] memory amounts) public payable returns (bool) {
-        // for (uint i = 0; i < eventIds_.length; i++){
-        //     require(bettingEvents[eventIds_[i]].status == State.OPEN, "Betting: Event not open");
-        // }
         require(bettingEvents[eventId_].status == State.OPEN, "Betting: Event not open");
         // TODO require a minimum total bet amounts
 
@@ -105,9 +66,6 @@ contract Betting is ERC1155Supply, Ownable {
         }
         require(msg.value >= amountsTotal, "Betting: Insufficient value to cover bets");
 
-        // safeBatchTransferFrom(address(this), msg.sender, resultIds_, amounts, "0x0");
-        
-        // another way
         for (uint i = 0; i < bettingEvents[eventId_].resultsIdsKeys.length; i++) {
             require(bettingEvents[eventId_].resultsIds[resultIds_[i]], "Betting: This result does not exist!");
         }
@@ -129,7 +87,7 @@ contract Betting is ERC1155Supply, Ownable {
 
     // Store the event result
     function store(uint256 eventId_, uint256 resultId_) public returns (bool) {
-        // Require accessControl
+        // TODO Require accessControl
         require(exists(resultId_), "Betting: This result does not exist!");
 
         bettingEvents[eventId_].finalResult = resultId_;
@@ -153,8 +111,7 @@ contract Betting is ERC1155Supply, Ownable {
     /// @return the potential or final winnings of msg.sender for a result id
     function computeWinnings(uint256 eventId_, uint256 resultId_) public view returns (uint256) {
         require(totalSupply(resultId_) != 0, "Betting: no bets on event");
-        // winnings = balanceOf(msg.sender, resultId_) * (1 + total_bets_on_resultId_ / total_bets_on_event)
-        
+
         uint256 winnings = balanceOf(msg.sender, resultId_) * totalBetsOnEvent(eventId_) / totalSupply(resultId_);
         winnings *= (10000 - fee)/10000;
 
@@ -196,10 +153,4 @@ contract Betting is ERC1155Supply, Ownable {
         payable(msg.sender).transfer(value_);
     }
 
-    // function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, ERC1155Receiver) returns (bool) {
-    //     return
-    //         interfaceId == type(IERC1155).interfaceId ||
-    //         interfaceId == type(ERC1155Receiver).interfaceId ||
-    //         super.supportsInterface(interfaceId);
-    // }
 }
